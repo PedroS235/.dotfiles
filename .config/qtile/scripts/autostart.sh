a968fb086a2d293956146e31bb73831438fa9e09
Pedro Soares <pmbs.123@gmail.com>
2024-02-21 08:47:42 +0100

    chore: remove

--- .config/qtile/scripts/autostart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Missing the nzxt controller
-megasync & # Start MegaSync
-bitwarden & # Start Bitwarden
-pactl set-default-sink alsa_output.pci-0000_08_00.4.analog-stereo & # Set default audio output
-# picom & # Start Picom
