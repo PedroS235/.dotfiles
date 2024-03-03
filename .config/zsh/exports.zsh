df1f651a971e37ac9bbcee66695a3f7993a1ed1a
Pedro Soares <pmbs.123@gmail.com>
2024-03-03 10:06:08 +0100

    fix: dotfiles path

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -4,4 +4,4 @@ export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border' # FZF
 export FZF_DEFAULT_COMMAND='fd --type f'
 export KEYTIMEOUT=1
 export XDG_CONFIG_HOME=$HOME/.config
-export DOTFILES=$HOME/dotfiles
+export DOTFILES=$HOME/.dotfiles
