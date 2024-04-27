479b64e5d4c7188f3c96c3cb094fd05cebb1b646
Pedro Soares <pmbs.123@gmail.com>
2024-04-27 11:51:20 +0200

    chore: update dotfiles

--- .config/zsh/sources.zsh
+++ .config/zsh/sources.zsh
@@ -1,10 +1,3 @@
 source $ZSH/oh-my-zsh.sh # oh-my-zsh
-
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh # FZF
-
-# [ -f "$HOME/.ghcup/env" ] && source "$HOME/.ghcup/env" # ghcup-env
-
 source "$HOME/.cargo/env"
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
