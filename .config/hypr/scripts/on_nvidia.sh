630c88c1d96e0946821aa2077c995e2a5e9cf826
Pedro Soares <pmbs.123@gmail.com>
2024-06-02 11:56:26 +0200

    feat: only source nvidia vars if nvdia is detected

---
+++ .config/hypr/scripts/on_nvidia.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -iq nvidia; then
+    ISNVIDIA=true
+else
+    ISNVIDIA=false
+fi
+
+if [[ "$ISNVIDIA" == true ]]; then
+    export LIBVA_DRIVER_NAME=nvidia
+    export GBM_BACKEND=nvidia-drm
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+fi
