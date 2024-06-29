60ff0de5ea95b27671f24e8b961ec6fe080e9ac7
Pedro Soares <pmbs.123@gmail.com>
2024-06-29 11:19:11 +0200

    feat(zsh): add .cargo/bin to be in PATH

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -1,9 +1,9 @@
-export ZSH="$HOME/.oh-my-zsh" # Path to oh-my-zsh folder
+export ZSH="$HOME/.oh-my-zsh"                                    # Path to oh-my-zsh folder
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border' # FZF
 export FZF_DEFAULT_COMMAND='fd --type f'
 export KEYTIMEOUT=1
 export XDG_CONFIG_HOME=$HOME/.config
 export DOTFILES=$HOME/.dotfiles
-export PATH=$PATH:$HOME/.local/share/bob/nvim-bin:$DOTFILES/bin/:$HOME/.local/bin
+export PATH=$PATH:$HOME/.local/share/bob/nvim-bin:$DOTFILES/bin/:$HOME/.local/bin:$HOME/.cargo/bin
 export SECOND_BRAIN_VAULT="$HOME/MEGA/Second_Brain_Vault/"
 export EDITOR=nvim
