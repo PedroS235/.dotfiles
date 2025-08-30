b70489e355fde1e70b579c8b4ffc3962f0cbabb7
Pedro Soares <pmbs.123@gmail.com>
2025-08-30 21:25:05 +0200

    fix

--- old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/arch/packages/ghostty.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Install Kitty as a backup
-sudo pacman -S --noconfirm --needed ghostty kitty
--- old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/old_dots/install/arch/packages/zsh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S --noconfirm --needed zsh
-
-# TODO: Missing Oh-my-zsh
